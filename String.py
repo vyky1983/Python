bfa4e5d304002356d55a919c88a398057a1c728b
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-19 14:05:04 +0300

    add oll

---
+++ String.py
@@ -0,0 +1,9 @@
+text = "Съешь ещё этих мягких француских булочек"
+print(len(text))
+print("ещё" in text)
+print(text.isdigit())
+print(text.islower())
+print(text.replace("ещё","ЕЩЕ"))
+
+for i in text:
+ print(i)
