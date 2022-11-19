9eacc68d3119e6521d18fdc13a0aea28451ff6db
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-19 13:33:52 +0300

    modified For

--- For.py
+++ For.py
@@ -1,9 +1,9 @@
-# for i in 1,2,3,4,5,:
-#  print(i**2)
+for i in 1,2,3,4,5,:
+ print(i**2)
 
-# list=[1,2,3,4, 14,5]
-# for i in list:
-#  print(i)
+list=[1,2,3,4, 14,5]
+for i in list:
+ print(i)
 
 r= range(2, 15, 2)
 for i in r:
