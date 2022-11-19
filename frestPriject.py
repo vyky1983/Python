ecd4d4b83e7886f7446cd467e50ae638fa47be4f
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-19 12:40:48 +0300

    Initial

---
+++ frestPriject.py
@@ -0,0 +1,24 @@
+print("hello world")
+
+a = 14
+b = 12, 25
+print(type(a))
+print(type(b))
+
+s = "Hello \nworld"
+print(s)
+print(a, b, s)
+
+print("{} - {} {} ".format(a, b, s))
+print(f"{a} - {b} {s} ")
+
+list = ["1", "2", "3", "hello"]
+coll = ['hell', '1', "2", ]
+print(list)
+
+print("Введите a")
+a = int(input())
+print("введите b")
+b = int(input())
+print("a", "", "b", "", a+b)
+
