013f294a04c186ae2bc96dbed0533cd668c49c3d
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-21 19:47:24 +0300

    Add new inf

---
+++ Уроки Python/Lesson Python/Work1/срезы.py
@@ -0,0 +1,17 @@
+text = "Съешь ещё этих мягких француских булочек"
+print(text[0])                        # c    
+print(text[1])                        # ъ
+# print(text[len(text)])              # Error
+print(text[len(text)-1])
+print(text[-5])
+print(text[:])
+print(text[:])
+print(text[len(text)-2])
+print(text[2:9])
+print(text[6:-18])
+print(text[0: len(text):6])
+print(text[::6])
+text= text[2:9]+text[-5] + text[:2]
+
+
+
