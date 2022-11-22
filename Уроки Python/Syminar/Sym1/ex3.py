fe15a9ed55328bc22445b89982b5fe5fa24306ad
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-22 20:27:08 +0300

    Task 3

--- Уроки Python/Syminar/Sym1/ex3.py
+++ Уроки Python/Syminar/Sym1/ex3.py
@@ -1,3 +1,9 @@
 # Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N.
 
 # -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5.
+
+number = int(input(" введите номер N --> "))
+# for i in range(-number, number+1):
+#     print(i, end=" ")
+
+print(*range(-number, number+1))
