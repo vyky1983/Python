b93e8c2f062c1234ec98d7154c9cf1cfaf386098
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-28 20:37:18 +0300

    add new file /plenty.py

---
+++ Уроки Python/Lesson Python/Lesson2/plenty.py
@@ -0,0 +1,12 @@
+color = { 'red', 'green', 'blue'}
+print(color)
+color.add('red')
+print(color)
+color.add('grey')
+print(color)
+color.remove('red')
+print(color)
+color.discard('red')
+print(color)
+color.clear()
+print(color)
