c7084efe7c89d5f16902051218f3d3685b97c573
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-28 14:13:46 +0300

    Add new file

--- Уроки Python/Lesson Python/Lesson2/ex1.py
+++ Уроки Python/Lesson Python/Lesson2/ex1.py
@@ -1,6 +1,8 @@
 color = ("red", "gren", "blue")
 data = open("file.txt", "w")
 data.writelines(color)  # Разделителей не будет
+data.write("\nLINE 312\n")
+data.write("\nLINE 513\n")
 data.close()
 
 exit()
