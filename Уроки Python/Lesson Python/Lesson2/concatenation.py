c8a3e9ea24ae866b575d4ce2053e441e9f9a8e40
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-28 19:24:14 +0300

    add concatenation.py

---
+++ Уроки Python/Lesson Python/Lesson2/concatenation.py
@@ -0,0 +1,10 @@
+def concatenation(*params):
+    res: str = ''
+    for item in params:
+        res += item
+    return res
+
+
+print(concatenation('a', 's', 'd', 'w'))  # asdw
+print(concatenation('a', '1', 'd', '2'))  # a1d2
+# print(concatenation(1, 2, 3, 4)) # Type Error
