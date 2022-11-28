278e36c5286ea74da4cc26abaee1fe0dc56dccf4
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-28 15:42:40 +0300

    add new inf in in file new_string

--- Уроки Python/Lesson Python/Lesson2/new_string.py
+++ Уроки Python/Lesson Python/Lesson2/new_string.py
@@ -2,6 +2,6 @@ def new_string(symbol, count =3):
     return symbol * count
 
 
-print(new_string('!', 5))
-print(new_string('!'))
-print(new_string('4'))      
+print(new_string('!', 5))  # !!!!!
+print(new_string('!'))     # !!!
+print(new_string('4'))     # 12 
