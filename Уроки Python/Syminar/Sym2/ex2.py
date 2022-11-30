2180e70df06e607ac2f9fb1401eb7660c19eae57
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-30 17:01:48 +0300

    add new file ex2, ex3

--- Уроки Python/Syminar/Sym2/ex2.py
+++ Уроки Python/Syminar/Sym2/ex2.py
@@ -2,3 +2,8 @@
 #  Пример 
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6:19} 
 
+n= int(input('Input number --> '))
+
+for i in range(1, n):
+ result = 3*i+1
+ print(result, end=(' '))
--- Уроки Python/Syminar/Sym2/ex3.py
+++ Уроки Python/Syminar/Sym2/ex3.py
@@ -1,1 +1,13 @@
 # Напишите программу, в которой пользователь будет задовать две строки, а программа определит количество вхождений одной строки.
+
+text = 'Я люблю Python люлюлюлюлюлю'
+
+char = input('Введите значение поиска: ')
+len_char = len(char)
+i = 0
+count = 0
+while i < len(text)-1:
+    if char.lower() == text[i:len_char+i].lower():
+        count += 1
+    i += 1
+print(count )
