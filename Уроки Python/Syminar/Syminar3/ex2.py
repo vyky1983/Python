e23c96ca6c666408c5b8bb04bb20263fb000030f
vyky1983 <veronica.cernenchii@mail.ru>
2022-12-01 20:06:21 +0300

    add new file ex2, ex3

--- Уроки Python/Syminar/Syminar3/ex2.py
+++ Уроки Python/Syminar/Syminar3/ex2.py
@@ -1,2 +1,12 @@
 # Реализуйте алгоритм задания случайных чисел без использования встройного генератора певдослучайных чисел.
 
+import time
+
+
+def my_random(max: int):
+    a = time.time()
+    tempo = round(a) % max
+    return tempo
+
+
+print(my_random(10))
