bd12480054f38dbb6c2dcf905b4dbed877150605
vyky1983 <veronica.cernenchii@mail.ru>
2022-12-06 10:58:44 +0300

    add new folder Sym4

--- Уроки Python/Lesson Python/Lesson2/ex1.py
+++ Уроки Python/Lesson Python/Lesson2/ex1.py
@@ -1,9 +1,9 @@
-# color = ("red", "gren", "blue3")
-# data = open("file.txt", "w")
-# #data.writelines(color)  # Разделителей не будет
-# data.write("\nLINE 312\n")
-# data.write("\nLINE 513\n")
-# data.close()
+color = ("red", "gren", "blue3")
+data = open("file.txt", "w")
+#data.writelines(color)  # Разделителей не будет
+data.write("\nLINE 312\n")
+data.write("\nLINE 513\n")
+data.close()
 
 
 path = "file.txt"
---
+++ Уроки Python/Lesson Python/Lesson3/map.py
@@ -0,0 +1,11 @@
+def select(f, col):
+   return [f(x) for x in col]
+
+def where(f, col):
+ return [x for x in col if f(x)]
+
+data="1 2 3 5 8 23 38".split()
+res=select(int, data)
+res=where(lambda x: not x%2,res)
+res=select(lambda x:(x,x**2), res)
+print(res)
